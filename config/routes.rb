7fcd3ca7b403769bbbbb1c583ac3571df3ed20a0
Ali El-Ashram <ali.elashram@gmail.com>
2013-08-21 03:41:30 -0700

    RESTful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Ticketee::Application.routes.draw do
+  root :to => "projects#index"
+  resources :projects
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +56,4 @@ Ticketee::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
