3d6149af90b73cd8a9a35a41987b266a4452437d
Ali El-Ashram <ali.elashram@gmail.com>
2013-08-23 01:56:16 -0700

    update login view

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -17,7 +17,8 @@ project = Project.where(name: "TextMate 2").first
 
 expect(page.current_url).to eql(project_url(project))
 
-
+title = "TextMate 2 - Projects - Ticketee"
+expect(find("title").native.text).to have_content(title)
 end
 
 scenario "can not create a project without a name" do 
